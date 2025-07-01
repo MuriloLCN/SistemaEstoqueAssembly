1905acac548c5bcf60b025216eec604654b9772b
Leandro Eugênio Farias Berton <95644934+Leandroefb@users.noreply.github.com>
2025-07-01 09:02:47 -0300

    corrigindo função de mostrar o banner + declaração de variáveis

--- sistema-estoque.s
+++ sistema-estoque.s
@@ -8,6 +8,7 @@
 */
 
 .section .data
+    # strings do banner de apresentação do sistema
     banner1:        .asciz  "\n   _____ _     _                             _        ______     _                         \n";
     banner2:        .asciz  "  / ____(_)   | |                           | |      |  ____|   | |                        \n";
     banner3:        .asciz  " | (___  _ ___| |_ ___ _ __ ___   __ _    __| | ___  | |__   ___| |_ ___   __ _ _   _  ___ \n";
@@ -17,16 +18,45 @@
     banner7:        .asciz  "                                                                             | |           \n";
     banner8:        .asciz  "                                                                             |_|           \n";
 
+    # strings de formatação
     formatoSTR:     .asciz  "%s"
     formatoINT:     .asciz  "%d"
     formatoFLOAT:   .asciz  "%f"
     formatoCHAR:    .asciz  "%c"
+    limpaTerminal:  .asciz  "clear"
+
+    # variáveis de trabalho
+    data_atual:             .int    0
+    tamanho_lista:          .int    0
+    lote_produto:           .int    0
+    tipo_produto:           .int    0
+    data_validade:          .int    0
+    quantidade_estoque:     .int    0
+    op_menu:                .int    0
+    resultado_comparacao:   .int    0
+    valor_compra:           .float  0.0
+    valor_venda:            .float  0.0
+
+.section .bss
+    .lcomm  nome_produto,   16  # 16 caracteres
+    .lcomm  fornecedor,     16
+    .lcomm  nome_aux,       16
+    .lcomm  ponteiro_prox,  4
+    .lcomm  no,             4
+    .lcomm  no_anterior,    4
+    .lcomm  novo_no,        4
+    .lcomm  inicio_lista,   4
+    .lcomm  arquivo_ptr,    4
 
 .section .text
 .globl main
 
 main:
+    pushl   $limpaTerminal      # limpando o terminal para a execução do programa
+    call    system          
+
     call mostra_banner
+
     jmp fim
 
 mostra_banner:
@@ -46,6 +76,9 @@ mostra_banner:
     call    printf
     pushl   $banner8
     call    printf
+
+    addl    $32, %esp           # desempilhando os endereços das 8 strings do banner
+    RET
     
 fim:
     pushl   $0
